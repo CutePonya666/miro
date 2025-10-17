99bad5fa5df3286e807239f6ec8120701def29ab
s_gorunovich <sg@digift.ru>
2025-10-17 18:34:42 +0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { BoardPage } from "@/features/board";
+
+function App() {
+  return (
+    <div>
+      <BoardPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/boards-list/index.tsx
@@ -0,0 +1,5 @@
+export { BoardCard } from "@/features/boards-list/board-card";
+
+export function BoardListPage() {
+  return <div>BoardListPage</div>;
+}
